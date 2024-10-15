ae230f6b7ab60332f0b2ef263c52d0a8b93bce45
yuhangch <i@yuhang.ch>
2024-10-15 22:44:38 +0800

    chore: update content.

--- src/plugins/remark-modified-time.mjs
+++ src/plugins/remark-modified-time.mjs
@@ -4,7 +4,8 @@ export function remarkModifiedTime() {
     return function (tree, file) {
         const filepath = file.history[0]
         const result = execSync(`cd src/content && git log -1 --pretty="format:%cI" ${filepath}`)
-        const lastModified = new Date( result.toString())
+
+        const lastModified = result.toString() ? new Date( result.toString()): new Date()
         file.data.astro.frontmatter.lastModified = lastModified
     }
 }
