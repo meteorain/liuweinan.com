6ade6b35bc905b9a77ad18689bee47783e770122
meteorain <liuweinan85@gmail.com>
2024-11-14 02:59:07 +0800

    UPDATE

--- src/loaders/moments.ts
+++ src/loaders/moments.ts
@@ -1,6 +1,6 @@
 
 //@ts-ignore
-import {API_URL,API_SECRET} from 'astro:env/server'
+import {API_URL} from 'astro:env/server'
 
 export interface MomentsLoaderOptions {
     /** URL of the feed */
@@ -10,53 +10,17 @@ export interface MomentsLoaderOptions {
     requestOptions?: RequestInit;
 }
 
-export interface MomentLoaderOptions {
-    /** URL of the feed */
-    id: string;
-    /** Extra options passed to the fetch request */
-    requestOptions?: RequestInit;
-}
+export function momentsLoader(): any {
+    const url = `${API_URL}/latest?limit=1000`;
 
-export interface Moment {
-    id: string;
-    body: string;
-    date: string;
-    location: string;
-    image: string;
-    tags: string[];
-}
-
-export function momentLoader(id:string): any {
-    const url = `${API_URL}/moments/${id}`;
-    const moment = fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${API_SECRET}`
-        }
-    }).then(res => res.json());
-
-
-    return {
-        name: "moment-loader",
-        load: async () => {
-            return moment
-        },
-    };
-}
-
-export function momentsLoader(page:number): any {
-    const url = `${API_URL}/moments?page=${page}`;
-
-    const moments = fetch(url, {
-        headers: {
-            'Authorization': `Bearer ${API_SECRET}`
-        }
-    }).then(res => res.json());
-
+    const moments = fetch(url).then(res => res.json()).then(data => 
+        data.filter((moment: Moment) => moment.uri && moment.uri.startsWith('/posts'))
+    );
 
     return {
         name: "moments-loader",
         load: async () => {
-            return moments
+            return {moments}
         },
     };
 }
